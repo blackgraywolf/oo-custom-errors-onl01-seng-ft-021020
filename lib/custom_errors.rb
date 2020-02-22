e7ad94aead7752960ca68d8c9756a424cab58940
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-22 21:23:22 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,17 +1,56 @@
-class Person
-  attr_accessor :partner, :name
 
-  def initialize(name)
-    @name = name
+class HighLine
+  # Internal HighLine errors.
+  module CustomErrors
+    # An error that responds to :explanation_key
+    class ExplainableError < StandardError
+      # Explanation key as Symbol or nil. Used to
+      # select the proper error message to be displayed.
+      # @return [nil, Symbol] explanation key to get the
+      #   proper error message.
+      def explanation_key
+        nil
+      end
+    end
+
+    # Bare Question error
+    class QuestionError < ExplainableError
+      # (see ExplainableError#explanation_key)
+      def explanation_key
+        nil
+      end
+    end
+
+    # Invalid Question error
+    class NotValidQuestionError < ExplainableError
+      # (see ExplainableError#explanation_key)
+      def explanation_key
+        :not_valid
+      end
+    end
+
+    # Out of Range Question error
+    class NotInRangeQuestionError < ExplainableError
+      # (see ExplainableError#explanation_key)
+      def explanation_key
+        :not_in_range
+      end
+    end
+
+    # Unconfirmed Question error
+    class NoConfirmationQuestionError < ExplainableError
+      # (see ExplainableError#explanation_key)
+      def explanation_key
+        nil
+      end
+    end
+
+    # Unavailable auto complete error
+    class NoAutoCompleteMatch < ExplainableError
+      # (see ExplainableError#explanation_key)
+      def explanation_key
+        :no_completion
+      end
+    end
   end
-
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
-
 end
-
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
